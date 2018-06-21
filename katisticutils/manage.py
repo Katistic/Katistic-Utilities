96788b86e7c445ff58b4c99be3f78852239a680d
Katistic <33645731+Katistic@users.noreply.github.com>
2018-06-22 09:57:08 +1000

    Update manage.py

--- katisticutils/manage.py
+++ katisticutils/manage.py
@@ -1,1 +1,13 @@
-
+def Join(DL, Seperater = ""):
+    if type(DL) != list:
+        raise TypeError("Expected variable "+str(list)+", got "+str(type(DL))+".")
+        return # In case
+    
+    Repeat = 0
+    StrDL = ""
+    for x in DL:
+        if Repeat != 0:
+            StrDL += Seperater
+        StrDL += str(x)
+    
+    return StrDL
