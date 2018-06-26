e93881f850cad971c662efe36e09edf4505cd5bb
Katistic <33645731+Katistic@users.noreply.github.com>
2018-06-26 13:16:51 +1000

    Update math.py

--- katisticutils/math.py
+++ katisticutils/math.py
@@ -1,1 +1,29 @@
+import math
 
+def getFactorsOf(Num):
+    if type(Num) != int:
+        raise TypeError("Expected type " + str(int) + ", got type " + type(Num))
+    
+    if not isWhole(Num):
+        raise Exception("Number must be whole.")
+    
+    Factors = [1, Num]
+    MaxFactor = math.ceil(Num / 2)
+    
+    for x in range(2, MaxFactor):
+        if x in Factors: continue
+        Ans = Num / x
+        if isWhole(Ans):
+            print("Found Factors:")
+            print(int(Ans))
+            print(int(x))
+            if not int(x) in Factors: Factors.append(int(x))
+            if not int(Ans) in Factors: Factors.append(int(Ans))
+    
+    return Factors
+
+def isWhole(Num):
+    if Num % 1 == 0:
+        return True
+    else:
+        return False
