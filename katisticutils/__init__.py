d734bac071d885e64f110839c14d20869d5caa9e
Katistic <33645731+Katistic@users.noreply.github.com>
2018-07-25 16:12:04 +1000

    Update __init__.py

--- katisticutils/__init__.py
+++ katisticutils/__init__.py
@@ -2,9 +2,14 @@ from . import kusocket as socket
 
 from . import math
 
-from . import file.Use
-from . import file.WriteToFile
-from . import file.ReadFromFile
+from . import file.Use as file.Use
+from . import file.WriteToFile as file.WriteToFile
+from . import file.ReadFromFile as file.ReadFromFile
 
-from .manage import join
-from .manage import isInt
+from .manage import join as manage.join
+from .manage import isInt as manage.isInt
+
+from .youtube import search_list as youtube.search_list
+from .youtube import search_playlist as youtube.search_playlist
+from .youtube import get_playlist_items as youtube.get_playlist_items
+from .youtube import Init as youtube.Init
