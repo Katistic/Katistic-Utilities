537f41dc88775b8d041c577ac5276285ffdb672f
Katistic <33645731+Katistic@users.noreply.github.com>
2018-07-25 16:17:46 +1000

    Update __init__.py

--- katisticutils/__init__.py
+++ katisticutils/__init__.py
@@ -6,10 +6,10 @@ from . import file.Use as file.Use
 from . import file.WriteToFile as file.WriteToFile
 from . import file.ReadFromFile as file.ReadFromFile
 
-from .manage import join as manage.join
-from .manage import isInt as manage.isInt
+from . import manage.join as manage.join
+from . import manage.isInt as manage.isInt
 
-from .youtube import search_list as youtube.search_list
-from .youtube import search_playlist as youtube.search_playlist
-from .youtube import get_playlist_items as youtube.get_playlist_items
-from .youtube import Init as youtube.Init
+from . import youtube.search_list
+from . import youtube.search_playlist
+from . import youtube.get_playlist_items
+from . import youtube.Init
