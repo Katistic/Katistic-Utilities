64440749de099041256caec73aeb2fdd8af5cb2d
Katistic <33645731+Katistic@users.noreply.github.com>
2018-08-10 13:29:14 +1000

    Reference change + manage.List.Shuffle func
    manage.Join() -> manage.List.Join()
    manage.List.Shuffle()
    
    Shuffles list

--- katisticutils/manage.py
+++ katisticutils/manage.py
@@ -1,16 +1,35 @@
-def Join(DL, Seperater = ""):
-    if type(DL) != list:
-        raise TypeError("KatisticUtils/manage.py: Expected variable "+str(list)+", got "+str(type(DL))+".")
-        return # In case
+import random
+
+class List:
+    def Join(self, DL, Seperater = ""):
+        if type(DL) != list:
+            raise TypeError("KatisticUtils/manage.py: Expected variable "+str(list)+", got "+str(type(DL))+".")
+            return # In case
+
+        Repeat = 0
+        StrDL = ""
+        for x in DL:
+            if Repeat != 0:
+                StrDL += Seperater
+            StrDL += str(x)
+
+        return StrDL
     
-    Repeat = 0
-    StrDL = ""
-    for x in DL:
-        if Repeat != 0:
-            StrDL += Seperater
-        StrDL += str(x)
-    
-    return StrDL
+    def Shuffle(self, List):
+        if type(List) != list:
+            raise TypeError("KatisticUtils/manage.py: Expected variable "+str(list)+", got "+str(type(DL))+".")
+            return
+        
+        NewList = []
+        while len(List) != 0:
+            Index = int(random.random() * len(List))
+            
+            try:
+                NewList.append(List.pop(Index))
+            except:
+                continue
+        
+        List = NewList8
 
 def IsInt(D):
     if type(D) == int:
